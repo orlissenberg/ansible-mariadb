ba42d143e402e62220f7111a0d2b8cadb0439899
Onno Lissenberg <orlissenberg@gmail.com>
2015-08-13 23:22:44 +0200

    First draft.

---
+++ README.md
@@ -0,0 +1,38 @@
+MariaDB Ansible Role
+====================
+
+Install and configure MariaDB 10 with Ansible.
+
+Requirements
+------------
+
+Debian 7
+
+Role Variables
+--------------
+
+-
+
+Dependencies
+------------
+
+No dependencies.
+
+Example Playbook
+----------------
+
+Including an example of how to use your role (for instance, with variables passed in as parameters) is always nice for users too:
+
+    - hosts: servers
+      roles:
+         - ansible-mariadb
+
+License
+-------
+
+MIT
+
+Notes
+-----
+
+-
---
+++ test.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+#sudo service mysql status
+#sudo apt-get remove --purge mariadb-server python-mysqldb -y
+#sudo rm -R /var/lib/mysql
+#sudo rm -R /var/log/mysql*
+#sudo nano /root/.my.cnf
+
+CURRENT_DIR=${PWD}
+TMP_DIR=/tmp/ansible-test
+mkdir -p $TMP_DIR 2> /dev/null
+
+# Create hosts inventory
+cat << EOF > $TMP_DIR/hosts
+[webservers]
+localhost ansible_connection=local
+EOF
+
+# Create group_vars for the webservers
+mkdir -p $TMP_DIR/group_vars 2> /dev/null
+cat << EOF > $TMP_DIR/group_vars/webservers
+
+mariadb_root_password: "ohhi"
+
+EOF
+
+# Create Ansible config
+cat << EOF > $TMP_DIR/ansible.cfg
+[defaults]
+roles_path = $CURRENT_DIR/../
+host_key_checking = False
+EOF
+
+# Create playbook.yml
+cat << EOF > $TMP_DIR/playbook.yml
+---
+
+- hosts: webservers
+  gather_facts: yes
+  sudo: yes
+
+  roles:
+    - ansible-mariadb
+EOF
+
+export ANSIBLE_CONFIG=$TMP_DIR/ansible.cfg
+
+# Syntax check
+ansible-playbook $TMP_DIR/playbook.yml -i $TMP_DIR/hosts --syntax-check
+
+# First run
+ansible-playbook $TMP_DIR/playbook.yml -i $TMP_DIR/hosts
+
+# Idempotence test
+ ansible-playbook $TMP_DIR/playbook.yml -i $TMP_DIR/hosts | grep -q 'changed=0.*failed=0' \
+ 	&& (echo 'Idempotence test: pass' && exit 0) \
+ 	|| (echo 'Idempotence test: fail' && exit 1)
+
+updatedb
